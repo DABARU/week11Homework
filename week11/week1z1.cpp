7fe9427eff15a1db6ead084d26667c56a1bc0701
DABARU <61595413+DABARU@users.noreply.github.com>
2020-06-16 19:34:15 +0700

    Add files via upload

---
+++ week11/week1z1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <ctime>
+#include <fstream>
+using namespace std;
+int   main()
+{
+    fstream f;
+    f.open("Z1.txt", ios::out | ios::trunc);
+
+    int const  n = 5;
+    int mx[n][n];
+
+    srand(time(NULL));
+
+    int min = 256;
+    int str = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            mx[i][j] = rand() % (min-1);
+   }
+}
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (mx[i][j] < min)
+            {
+                min = mx[i][j];
+                str = i;
+      }
+
+    }
+ }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        mx[str][i] = 0;
+
+    }
+}
